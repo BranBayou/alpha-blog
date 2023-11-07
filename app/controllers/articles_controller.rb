c67bfb2dd45e192cb9640f6c5793e7d9453998e7
BranBayou <berhan.baye@gmail.com>
2023-11-07 13:55:33 +0300

    Update contorller file

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,23 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def create
+     @article = Article.new(article_params)
+     if @article.save
+      flash[:notice] = "Article was created successfully"
+      redirect_to article_path(@article)
+     else
+      redirect_to new_article_path
+     end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
